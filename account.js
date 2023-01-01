0894d6d36589c950fd637db6c5778083c915db68
Jennifer Fonti <jennifer.fonti002@gmail.com>
2023-01-01 16:18:22 +0100

    personalize website

--- account.js
+++ account.js
@@ -1,10 +1,53 @@
 
 function loadAccount(){
-    if(localStorage.getItem("name")==''||localStorage.getItem("name")=='null'){
+    if(localStorage.getItem("name") === null){
         console.log("ciao");
         window.location.replace("../ecommerce/sign_in.html");
+    }else{
+        document.getElementById("greetings").innerHTML="HELLO<br><i>"+localStorage.getItem("name")+"</i>";
+    }
+    if(localStorage.getItem("hsize") !== null || localStorage.getItem("hcolor") !== null){
+        set_header();
     }
 
-    console.log(localStorage.getItem("name"));
-    document.getElementById("greetings").innerHTML="HELLO<br><i>"+localStorage.getItem("name")+"</i>";
+}
+function update_header(){
+    let size = document.getElementById("hsize").value;
+    let color = document.getElementById("hcolor").value;
+
+    if(size != 0){
+        if(size<1 || size>5){
+            alert("size must be >0 and <6");
+            return false;
+        }else{
+            localStorage.setItem('hsize',size);
+            localStorage.setItem('hcolor',color);
+            set_header();
+            return true;
+        }
+    }else{
+        localStorage.setItem('hcolor',color);
+        set_header();
+        return true;
+    }
+}
+function back_original(){
+    localStorage.setItem('hcolor','#e6e6e6');
+    localStorage.setItem('hsize','2');
+    set_header();
+}
+function set_header(){
+    let header =  document.getElementById("header");
+    header.style.backgroundColor = localStorage.getItem('hcolor');
+    
+    if(localStorage.getItem("hsize") === null){
+        console.log("size not found")
+    }else{
+        let size = localStorage.getItem('hsize')+"rem";
+        console.log(size)
+        document.querySelectorAll("div.header > a").forEach((a) => {
+            console.log(a);
+            a.style.fontSize = size;
+        });
+    }
 }
--- general.js
+++ general.js
@@ -1,24 +1,8 @@
-const queryString = window.location.search;
-console.log(queryString)
-
-const urlParams = new URLSearchParams(queryString);
-
-const title = urlParams.get('title')
-console.log(title);
-
-const img = urlParams.get('img')
-console.log(img);
-
-const price = urlParams.get('price')
-console.log(price);
-
-function pageOnLoad(){
-    document.getElementById('prod_title').innerHTML='FIAMMETTA GIANFANTI <br><i>'+title+'</i>';
-    document.getElementById('prod_image').src='img/'+img;
-    document.getElementById('prod_price').innerHTML='£'+price;
+function loadPage(){
     if(localStorage.getItem("hsize") !== null || localStorage.getItem("hcolor") !== null){
         set_header();
     }
+
 }
 function set_header(){
     let header =  document.getElementById("header");
@@ -34,7 +18,4 @@ function set_header(){
             a.style.fontSize = size;
         });
     }
-}
-function add_cart(){
-    localStorage.setItem(img, price);
 }
